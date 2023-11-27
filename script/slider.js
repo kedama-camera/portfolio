cae77c2d4c0d650738e487a62b2d782af7b25a85
Kenji Sonohara <k_sonohara@ewings.biz>
2023-11-27 19:19:54 +0900

    画像だけスライドするように修正。不要コード削除。

--- script/slider.js
+++ script/slider.js
@@ -2,10 +2,7 @@
 class Slide {
     constructor(el) {
         this.DOM = { el: el };
-        this.DOM.title = this.DOM.el.querySelector(".slide__title > span");
-        this.DOM.subtitle = this.DOM.el.querySelector(".slide__subtitle > span");
-        this.DOM.imgWrapper = this.DOM.el.querySelector(".slide__img-wrap");
-        this.DOM.revealer = this.DOM.el.querySelector(".slide__img-reveal");
+        this.DOM.img = this.DOM.el.querySelector(".slide__img");
 
         this.config = {
             duration: 0.6,
@@ -25,59 +22,22 @@ class Slide {
         return new Promise((resolve, reject) => {
             console.log(action, direction);
 
-            const revealerOptions = {
-                ease: this.config.ease,
-                delay: action === "hide" ? 0 : this.config.duration / 2,
-                onComplete: resolve
-            };
-
             const defaultOptions = {
                 delay: action === "hide" ? 0 : this.config.duration / 2,
                 ease: this.config.ease,
                 opacity: action === "hide" ? 0 : 1
             };
+            const imgOptions = Object.assign({ onComplete: resolve }, defaultOptions);
 
-            const titleOptions = Object.assign({}, defaultOptions);
-            const subtitleOptions = Object.assign({}, defaultOptions);
-            const imgOptions = Object.assign({}, defaultOptions);
-
-            revealerOptions.startAt =
-                action === "hide"
-                    ? { x: direction === "left" ? "-100%" : "100%", y: "0%" }
-                    : { x: "0%", y: "0%" };
-            revealerOptions.x =
-                action === "hide" ? "0%" : direction === "left" ? "100%" : "-100%";
             imgOptions.startAt =
                 action === "show"
                     ? { opacity: 0, x: direction === "left" ? "-10%" : "10%" }
                     : {};
+
             imgOptions.x =
                 action === "hide" ? (direction === "left" ? "10%" : "-10%") : "0%";
 
-            titleOptions.startAt =
-                action === "show"
-                    ? { opacity: 0, y: action === "show" ? "-100%" : "0%" }
-                    : {};
-            titleOptions.y = action === "hide" ? "100%" : "0%";
-            titleOptions.opacity = action === "hide" ? 0 : 1;
-
-            subtitleOptions.startAt =
-                action === "show" ? { opacity: 0, y: "-100%" } : {};
-            subtitleOptions.y = action === "hide" ? "100%" : "0%";
-            subtitleOptions.opacity = action === "hide" ? 0 : 1;
-            subtitleOptions.delay = action === "hide" ? 0 : this.config.duration;
-
-            // Toggling the revealer.
-
-            console.log(titleOptions);
-
-            // Moving & fading the image wrappper.
-
-            gsap.to(this.DOM.revealer, this.config.duration, revealerOptions);
-            gsap.to(this.DOM.subtitle, this.config.duration, subtitleOptions);
-            gsap.to(this.DOM.imgWrapper, this.config.duration, imgOptions);
-            gsap.to(this.DOM.title, this.config.duration, titleOptions);
-            // Moving & fading the title and number.
+            gsap.to(this.DOM.img, imgOptions);
         });
     }
 }
@@ -94,13 +54,12 @@ class Slideshow {
         );
 
         this.slideCount = this.slides.length;
-        this.current = 0;
+        this.current = (this.slideCount - 1);
         this.init();
     }
 
     init() {
         this.slides[this.current].setCurrent();
-        this.slides[this.current].show("right");
         this.initEvents();
     }
 
